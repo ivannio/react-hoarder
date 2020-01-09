ea3e851a2d8e9ec567a39cf7402d2ab91fe7d57e
Ivan Phelps <ivanniothemannio@gmail.com>
2020-01-08 19:05:47 -0600

    setup complete

---
+++ src/App/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.scss';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>APP</h1>
+      <button className="btn btn-outline-success">We bootstrappin?</button>
+    </div>
+  );
+}
+
+export default App;
