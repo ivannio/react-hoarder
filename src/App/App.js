fa628c6b359bc913e46d8c75e6b5396b8f5f5c03
Ivan Phelps <ivanniothemannio@gmail.com>
2020-01-08 19:35:12 -0600

    proptypes added

--- src/App/App.js
+++ src/App/App.js
@@ -1,13 +1,47 @@
 import React from 'react';
+// import {
+//   BrowserRouter as Router,
+//   Route,
+//   Redirect,
+//   Switch,
+// } from 'react-router-dom';
+import firebase from 'firebase/app';
+import 'firebase/auth';
+
+import MyNavBar from '../components/shared/MyNavBar/MyNavBar';
+import Auth from '../components/pages/Auth/Auth';
+
 import './App.scss';
 
-function App() {
-  return (
-    <div className="App">
-      <h1>APP</h1>
-      <button className="btn btn-outline-success">We bootstrappin?</button>
-    </div>
-  );
+class App extends React.Component {
+  state = {
+    authed: false,
+  };
+
+  componentDidMount() {
+    this.removeListener = firebase.auth().onAuthStateChanged((user) => {
+      if (user) {
+        this.setState({ authed: true });
+      } else {
+        this.setState({ authed: false });
+      }
+    });
+  }
+
+  componentWillUnmount() {
+    this.removeListener();
+  }
+
+  render() {
+    const { authed } = this.state;
+    return (
+      <div className="App">
+        <MyNavBar authed={authed}/>
+        <Auth authed={authed}/>
+      </div>
+    );
+  }
+  
 }
 
 export default App;
---
+++ src/components/shared/MyNavBar/MyNavBar.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+class MyNavBar extends React.Component {
+  render() {
+    return (
+      <></>
+    );
+  }
+}
+
+export default MyNavBar;
